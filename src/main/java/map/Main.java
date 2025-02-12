e91b17e9692537db9661234d1f3844e83fc15981
George Secure <91793381+ApyrGeo@users.noreply.github.com>
2025-02-12 12:48:34 +0200

    Update Main.java

--- src/main/java/map/Main.java
+++ src/main/java/map/Main.java
@@ -10,16 +10,7 @@ public class Main {
         }
 
 
-//        Service s = new Service(
-//                new UtilizatorDbRepository("jdbc:postgresql://localhost:5432/socialnetwork",
-//                        "postgres",
-//                        "Injection17_DROP_TABLE",
-//                        new UtilizatorValidator()),
-//                new PrietenieDbRepository("jdbc:postgresql://localhost:5432/socialnetwork",
-//                        "postgres",
-//                        "Injection17_DROP_TABLE",
-//                        new PrietenieValidator()));
-//        new UI(s).Run();
+
 
 
     }
